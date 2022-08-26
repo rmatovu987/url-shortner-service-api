cd0108ee8dd7f5b178c4563886ee0288ca0c1198
rmatovu987 <rmatovu987@gmail.com>
2022-08-26 12:06:49 +0300

    Deployment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
 
   def send_to_url
     url = params[:short_url]
-    link = Url.find_sole_by(['short_url = ?', 'http://18.141.56.186:3000/app/'+url])
+    link = Url.find_sole_by(['short_url = ?', 'http://18.141.56.186/url/'+url])
     link.clicks += 1
     link.save
     redirect_to link.original_url, allow_other_host: true
