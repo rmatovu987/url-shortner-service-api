becdc8af6bb6dd07dcc460e34f55a6a2d09c0e14
rmatovu987 <rmatovu987@gmail.com>
2022-08-27 12:34:43 +0300

    passenger

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,7 @@ Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     devise_for :users,
                controllers: {
-                 session: 'sessions',
-                 registrations: 'registrations'
+                 sessions: 'sessions'
                },
                path_names: { sign_in: :login }
 
